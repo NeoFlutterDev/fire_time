ccc3c62680366d2dc81bb794429175efc26071f0
NeoFlutterDev <neojfc1@gmail.com>
2024-05-19 18:45:59 +0100

    flashcard work

--- lib/flashcards.dart
+++ lib/flashcards.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:hive/hive.dart';  
 import 'package:intl/intl.dart';
 
@@ -7,7 +6,7 @@ checkEmptyFolder() {
   var folders = Hive.box('FlashCards');
   if (folders.isEmpty) {
     DateTime now = DateTime.now();
-    folders.add(['New Folder', (DateFormat('yyyy-MM-dd').format(now.toLocal())), ['Empty flashcard', 'Empty']]);
+    folders.add(['New Folder', (DateFormat('yyyy-MM-dd').format(now.toLocal())), [['Empty flashcard', 'Empty']]]);
   }
 }
 
@@ -16,40 +15,17 @@ checkEmptyFlashCard(index) {
   if (folders.getAt(index).isEmpty) {
     folders.clear();
     DateTime now = DateTime.now();
-    folders.add(['New Folder', (DateFormat('yyyy-MM-dd').format(now.toLocal())), ['Empty flashcard', 'Empty']]);
+    folders.add(['New Folder', (DateFormat('yyyy-MM-dd').format(now.toLocal())), [['Empty flashcard', 'Empty']]]);
   }
 }
 
-class TextFieldForm extends StatefulWidget {
-  const TextFieldForm({super.key});
-
-  @override
-  State<TextFieldForm> createState() => _TextFieldFormState();
-}
-
-class _TextFieldFormState extends State<TextFieldForm> {
-  final myController = TextEditingController();
-  @override 
-  void dispose() {
-    myController.dispose();
-    super.dispose();
+createSides(index) {
+  var folders = Hive.box('FlashCards');
+  List<int> side = [];
+  for (int i = 0; i < (folders.getAt(index)[2]).length - 2; i++) {
+    side.add(0);
   }
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      style: const TextStyle(
-        color: Colors.white
-      ),
-      controller: myController,
-      maxLength: 20,
-      decoration: const InputDecoration(
-        hintText: 'Enter folder name here',
-        hintStyle: TextStyle(
-          color: Colors.white
-        )
-      )
-    );
-  }
+  return side;
 }
 
 class FlashCards extends StatefulWidget {
@@ -85,12 +61,23 @@ class _FlashCardsState extends State<FlashCards> {
                       )
                     ),
                     content: 
-                      TextFormField(),
+                      TextField(
+                        style: const TextStyle(
+                          color: Colors.white
+                        ),
+                        controller: myController,
+                        maxLength: 20,
+                        decoration: const InputDecoration(
+                          hintText: 'Enter folder name here',
+                          hintStyle: TextStyle(
+                            color: Colors.white
+                          )
+                        )
+                      ),   
                     actions: [
                       TextButton(
                         onPressed:() => [
-                          folders.add([myController.text, (DateFormat('yyyy-MM-dd').format(now.toLocal())), []]),
-                          myController.dispose(),
+                          folders.add([myController.text, (DateFormat('yyyy-MM-dd').format(now.toLocal())), [['Empty flashcard', 'Empty']]]),
                           setState(() {iteration++;}),
                           Navigator.pop(context)
                         ],
@@ -240,31 +227,135 @@ class _FlashCardsState extends State<FlashCards> {
 }
 
 class FlashCardFolder extends StatefulWidget {
+  const FlashCardFolder({super.key, required this.flashcardsLocation, required this.title});
   final String title;
   final int flashcardsLocation;
-  const FlashCardFolder({super.key, required this.flashcardsLocation, required this.title});
 
   @override
   State<FlashCardFolder> createState() => _FlashCardFolderState();
 }
 
 class _FlashCardFolderState extends State<FlashCardFolder> {
-  var folders = Hive.box("FlashCards");
+  var box = Hive.openBox('FlashCards');
+  var folders = Hive.box('FlashCards');
+  final flashcardFront = TextEditingController();
+  final flashcardBack = TextEditingController();
+  DateTime now = DateTime.now();
+  int iteration = 0;
+  List<int> side = [1];
   
   @override
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
           children: [
-            ListView.builder(
-              itemCount: folders.get(widget.flashcardsLocation).length,
-              itemBuilder: (context, index) {
-                return Card(
-                  color: Colors.black,
-                  child: Text(folders.get(widget.flashcardsLocation)[0].toString())
-                );
-              },
+            ElevatedButton(
+              onPressed:() => (
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    backgroundColor: Colors.black,
+                    title: const Text(
+                      'Flashcard creation',
+                      style: TextStyle(
+                        color: Colors.white
+                      )
+                    ),
+                    content: 
+                    Column (
+                      children: [
+                        TextField(
+                          style: const TextStyle(
+                            color: Colors.white
+                          ),
+                          controller: flashcardFront,
+                          maxLength: 20,
+                          decoration: const InputDecoration(
+                            hintText: 'Enter flashcard front here',
+                            hintStyle: TextStyle(
+                              color: Colors.white
+                            )
+                          )
+                        ),
+                        TextField(
+                          style: const TextStyle(
+                            color: Colors.white
+                          ),
+                          controller: flashcardBack,
+                          maxLength: 20,
+                          decoration: const InputDecoration(
+                            hintText: 'Enter flashcard back here',
+                            hintStyle: TextStyle(
+                              color: Colors.white
+                            )
+                          )
+                        ),
+                      ],
+                    ), 
+                    actions: [
+                      TextButton(
+                        onPressed:() => [
+                          folders.getAt(widget.flashcardsLocation)[1] = DateFormat('yyyy-MM-dd').format(now.toLocal()),
+                          folders.getAt(widget.flashcardsLocation)[2].add([flashcardFront.text, flashcardBack.text]),
+                          setState(() {iteration++;}),
+                          Navigator.pop(context)
+                        ],
+                        child: const Text(
+                          'Create',
+                          style: TextStyle(
+                            color: Colors.blue
+                          )
+                        )
+                      ),
+                      TextButton(
+                        onPressed:() => Navigator.pop(context),
+                        child: const Text(
+                          'Cancel',
+                          style: TextStyle(
+                            color: Colors.blue
+                          )
+                        )
+                      )
+                    ],
+                  )
+                )
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black
+              ),
+              child: const Text(
+                'Make new folder',
+                style: TextStyle(
+                  color: Colors.blue
+                )
+              )
+            ),
+            Expanded(
+              child:
+              ListView.builder(
+                itemCount: (folders.getAt(widget.flashcardsLocation)[2]).length,
+                itemBuilder: (context, index) {
+                  if (side == []) {
+                    side = createSides(widget.flashcardsLocation);
+                  }
+                  return Card(
+                    clipBehavior: Clip.hardEdge,
+                    child: InkWell(
+                      splashColor: const Color.fromARGB(255, 0, 0, 0).withAlpha(30),
+                      onTap: () {
+                        side[index] = side[index] ^ 1;
+                        setState(() {});
+                      },
+                      child: SizedBox(
+                        height: 200,
+                        child: Center(child: Text(folders.getAt(widget.flashcardsLocation)[2][index][(side[index])].toString())),
+                      ),
+                    ),
+                  );
+                },
+              )
             )
           ],
         )
